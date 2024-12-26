621bbf2a5fad189f5639b370c933461b9ff6bcd0
TA1851 <taosaka1851@gmail.com>
2024-12-26 15:40:57 +0900

    name zoom-in create

---
+++ src/components/vue/myname.vue
@@ -0,0 +1,30 @@
+<template>
+  <div
+    data-aos="zoom-in"
+    data-aos-duration="3000"
+    data-aos-delay="700"
+  >
+    <h1 class="text-2xl text-center text-yellow-50 pt-10 mt-5 customFont:text-xl">
+      <span class="inline-block text-2xl bg-gradient-to-r
+        from-gray-400 via-emerald-500 via-green-500 via-emerald-500 to-gray-400
+        bg-clip-text text-transparent w-fit"
+      >
+        Tatuhiko Osaka's Portfolio
+      </span>
+    </h1>
+      <p class="text-center text-white">
+        welcome to my portfolio site
+      </p>
+  </div>
+</template>
+
+<script>
+import AOS from 'aos'
+import 'aos/dist/aos.css'
+
+export default {
+  mounted() {
+    AOS.init()
+  }
+}
+</script>
