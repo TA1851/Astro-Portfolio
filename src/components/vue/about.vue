26e03e4f1b0912971d8757f93522d2c1e204d521
TA1851 <taosaka1851@gmail.com>
2025-01-01 17:12:12 +0900

    errorと警告の解除

--- src/components/vue/about.vue
+++ src/components/vue/about.vue
@@ -1,25 +1,30 @@
 <template>
-  <div
-    data-aos="fade-right"
-    data-aos-duration="1800"
-    data-aos-delay="400"
-    data-aos-once="true"
-  >
-  <h2 class="secondary-title text-center custom:mx-5 text-5xl text-green-300">ABOUT ME</h2>
-  </div>
-  <div
-    data-aos="fade-up"
-    data-aos-duration="1800"
-    data-aos-delay="400"
-    data-aos-once="true">
-    <h2 class="text-xl mt-20 text-gray-100 text-center">プログラミングを学び始めたきっかけ</h2>
-      <p class="mt-5 text-gray-100">
-        札幌出身のエンジニア。<br>
-        私はサイバー大学というオンライン大学で初めてプログラミングと出会いました。
-        それまではプログラミングとは無縁の生活を送っていましたが、その魅力に惹かれ、積極的に学習を始めることにしました。
-        実務ではExcelVBAで自動化ツールを開発したり、Pythonでデータ収集やGUI操作自動化ツールの開発を行ってきました。
-        現在はフロントエンドの技術を学んでいます。
-      </p>
+  <div class="about-container">
+    <div
+      data-aos="fade-right"
+      data-aos-duration="1800"
+      data-aos-delay="400"
+      data-aos-once="true"
+    >
+      <h2 class="secondary-title text-center custom:mx-5 text-5xl text-green-300">ABOUT ME</h2>
+    </div>
+    <div
+      data-aos="fade-up"
+      data-aos-duration="1800"
+      data-aos-delay="400"
+      data-aos-once="true"
+    >
+      <h2 class="text-xl mt-20 text-gray-100 text-center">
+        プログラミングを学び始めたきっかけ
+      </h2>
+        <p class="mt-5 text-gray-100">
+          札幌出身のエンジニア。<br>
+          私はサイバー大学というオンライン大学で初めてプログラミングと出会いました。
+          それまではプログラミングとは無縁の生活を送っていましたが、その魅力に惹かれ、積極的に学習を始めることにしました。
+          実務ではExcelVBAで自動化ツールを開発したり、Pythonでデータ収集やGUI操作自動化ツールの開発を行ってきました。
+          現在はフロントエンドの技術を学んでいます。
+        </p>
+    </div>
   </div>
 </template>
 
--- src/components/vue/myname.vue
+++ src/components/vue/myname.vue
@@ -1,20 +1,22 @@
 <template>
-  <div
-    data-aos="zoom-in"
-    data-aos-duration="3000"
-    data-aos-delay="700"
-  >
-    <h1 class="text-2xl text-center text-yellow-50 pt-10 mt-5 customFont:text-xl">
-      <span class="inline-block text-2xl bg-gradient-to-r
-        from-gray-400 via-emerald-500 via-green-500 via-emerald-500 to-gray-400
-        bg-clip-text text-transparent w-fit"
-      >
-        Tatuhiko Osaka's Portfolio
-      </span>
-    </h1>
-      <p class="text-center text-white">
-        welcome to my portfolio site
-      </p>
+  <div class="top-container">
+    <div
+      data-aos="zoom-in"
+      data-aos-duration="3000"
+      data-aos-delay="700"
+    >
+      <h1 class="text-2xl text-center text-yellow-50 pt-10 mt-5 customFont:text-xl">
+        <span class="inline-block text-2xl bg-gradient-to-r
+          from-gray-400 via-emerald-500 via-green-500 via-emerald-500 to-gray-400
+          bg-clip-text text-transparent w-fit"
+        >
+          Tatuhiko Osaka's Portfolio
+        </span>
+      </h1>
+        <p class="text-center text-white">
+          welcome to my portfolio site
+        </p>
+    </div>
   </div>
 </template>
 
